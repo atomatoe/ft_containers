8ae6d68eecdc00a16a030b68a08bfcaaca7329e0
Antwan Tomatoes <atomatoe@si-f3.kzn.21-school.ru>
2020-12-14 16:44:58 +0300

    queue done

--- main.cpp
+++ main.cpp
@@ -6,111 +6,89 @@
 /*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/13 14:57:41 by atomatoe          #+#    #+#             */
-/*   Updated: 2020/12/14 15:58:06 by atomatoe         ###   ########.fr       */
+/*   Updated: 2020/12/14 16:44:47 by atomatoe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "vector.hpp"
+// #include "vector.hpp"
 #include "stack.hpp"
-#include "list.hpp"
+#include "queue.hpp"
+// #include "list.hpp"
 #include <vector>
 #include <stack>
 #include <list>
+#include <queue>
 
 int main()
 {
-     std::cout << std::endl;
-     std::cout << "| MY STACK: |" << std::endl;
-     ft::stack<int> my;
-     my.push(10);
-     my.pop();
-     my.push(20);
-     my.push(30);
-     my.push(40);
-     my.push(50);
-     my.pop();
-     std::cout << "my size: " << my.size() << std::endl;
-     std::cout << "my top: " << my.top() << std::endl;
-     ft::stack<int> my2;
-     if(my > my2)
-         std::cout << "TESTING" << std::endl;
-     std::cout << std::endl;
-     std::cout << "| STANDART STACK: |" << std::endl;
-     std::stack<int> standart;
-     standart.push(10);
-     standart.pop();
-     standart.push(20);
-     standart.push(30);
-     standart.push(40);
-     standart.push(50);
-     standart.pop();
-     std::cout << "st size: " << standart.size() << std::endl;
-     std::cout << "st top: " << standart.top() << std::endl;
-     std::stack<int> standart2;
-     if(standart > standart2)
-         std::cout << "TESTING" << std::endl;
-     std::cout << std::endl;
-
-
-    std::cout << std::endl;
-
-
-    // std::cout << "| MY LIST: |" << std::endl;
-    // ft::List<int> my;
-    // my.push(10);
-    // my.pop();
-    // my.push(20);
-    // my.push(30);
-    // my.push(40);
-    // my.push(50);
-    // my.pop();
-    // std::cout << "my size: " << my.size() << std::endl;
-    // std::cout << "my top: " << my.top() << std::endl;
-    // ft::Stack<int> my2;
-    // if(my < my2)
-    //     std::cout << "TESTING" << std::endl;
+    //  std::cout << std::endl;
+    //  std::cout << "| MY STACK: |" << std::endl;
+    //  ft::stack<int> my;
+    //  my.push(10);
+    //  my.pop();
+    //  my.push(20);
+    //  my.push(30);
+    //  my.push(40);
+    //  my.push(50);
+    //  my.pop();
+    //  std::cout << "my size: " << my.size() << std::endl;
+    //  std::cout << "my top: " << my.top() << std::endl;
+    //  ft::stack<int> my2;
+    //  if(my > my2)
+    //      std::cout << "TESTING" << std::endl;
+    //  std::cout << std::endl;
+    //  std::cout << "| STANDART STACK: |" << std::endl;
+    //  std::stack<int> standart;
+    //  standart.push(10);
+    //  standart.pop();
+    //  standart.push(20);
+    //  standart.push(30);
+    //  standart.push(40);
+    //  standart.push(50);
+    //  standart.pop();
+    //  std::cout << "st size: " << standart.size() << std::endl;
+    //  std::cout << "st top: " << standart.top() << std::endl;
+    //  std::stack<int> standart2;
+    //  if(standart > standart2)
+    //      std::cout << "TESTING" << std::endl;
+    //  std::cout << std::endl;
 
 
 
-    // std::cout << std::endl;
+    //  std::cout << std::endl;
+    //  std::cout << "| MY QUEUE: |" << std::endl;
+    //  ft::queue<int> my;
+    //  my.push(10);
+    //  my.pop();
+    //  my.push(20);
+    //  my.push(30);
+    //  my.push(40);
+    //  my.push(50);
+    //  my.pop();
+    //  std::cout << "my size: " << my.size() << std::endl;
+    //  ft::queue<int> my2;
+    //  if(my > my2)
+    //      std::cout << "TESTING" << std::endl;
+    //  std::cout << std::endl;
+    //  std::cout << "| STANDART QUEUE: |" << std::endl;
+    //  std::queue<int> standart;
+    //  standart.push(10);
+    //  standart.pop();
+    //  standart.push(20);
+    //  standart.push(30);
+    //  standart.push(40);
+    //  standart.push(50);
+    //  standart.pop();
+    //  std::cout << "st size: " << standart.size() << std::endl;
+    //  std::queue<int> standart2;
+    //  if(standart > standart2)
+    //      std::cout << "TESTING" << std::endl;
+    //  std::cout << std::endl;
 
 
 
-    // std::cout << "| STANDART LIST: |" << std::endl;
-    // std::list<int> standart;
-    // standart.push(10);
-    // standart.pop();
-    // standart.push(20);
-    // standart.push(30);
-    // standart.push(40);
-    // standart.push(50);
-    // standart.pop();
-    // std::cout << "st size: " << standart.size() << std::endl;
-    // std::cout << "st top: " << standart.top() << std::endl;
-    // std::stack<int> standart2;
-    // if(standart < standart2)
-    //     std::cout << "TESTING" << std::endl;
 
 
-    // std::cout << std::endl;
+
+
 }
-
-
-
-
-    // name.push_back(50);
-    // name.push_back(60);
-    // name.push_back(70);
-    // name.pop_back();
-    // name.assign(2, 52);
-    // name.push_back(60);
-    // name.push_back(70);
-    // std::cout << "vector size: " << name.size() << std::endl;
-    // // std::cout << "vector max size: " << name.max_size() << std::endl;
-    // std::cout << "vector empty: " << name.empty() << std::endl;
-    // // std::cout << "capacity: " << name.capacity() << std::endl;
-    // std::cout << "standart vector: " << std::endl;
-    // // name.clear();
-    // for(std::vector<int>::iterator i = name.begin(); i != name.end(); i++)
-    //     std::cout << *i << std::endl;
-    // std::vector<int> name3(5);;
---
+++ queue.hpp
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/14 16:02:42 by atomatoe          #+#    #+#             */
+/*   Updated: 2020/12/14 16:43:25 by atomatoe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef QUEUE_HPP
+# define QUEUE_HPP
+
+#include <iostream>
+#include <deque>
+
+namespace ft
+{
+template <class T, class Container = std::deque<T> >
+class queue
+{
+public:
+    typedef  T			value_type;
+    typedef  size_t		size_type;
+    typedef Container	container_type;
+    explicit queue (const container_type& ctnr = container_type())
+    {
+        _container = ctnr;
+    }
+    ~queue() {} ;
+    bool empty() const
+    {
+		return(_container.empty());
+    }
+    size_type size() const
+    {
+        return(_container.size());
+    }
+    const value_type& front() const
+    {
+        return(_container.front());
+    }
+    value_type& front()
+    {
+        return(_container.front());
+    }
+    value_type& back()
+    {
+		return(_container.back());
+    }
+    const value_type& back() const
+    {
+        return(_container.back());
+    }
+    void push (const value_type& val)
+    {
+        _container.push_back(val);
+    }
+    void pop()
+    {
+        _container.pop_back();
+    }
+
+protected:
+    container_type _container;
+
+};
+template <class T, class Container>
+  bool operator == (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() == rhs.size());
+  }
+template <class T, class Container>
+  bool operator != (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() != rhs.size());
+  }
+template <class T, class Container>
+  bool operator <  (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() < rhs.size());
+  }
+template <class T, class Container>
+  bool operator <= (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() <= rhs.size());
+  }
+template <class T, class Container>
+  bool operator >  (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() > rhs.size());
+  }
+template <class T, class Container>
+  bool operator >= (const ft::queue<T,Container>& lhs, const ft::queue<T,Container>& rhs)
+  {
+	  return(lhs.size() >= rhs.size());
+  }
+
+};
+
+#endif
